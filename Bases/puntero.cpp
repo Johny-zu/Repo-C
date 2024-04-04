585dadcd0c968a1d921119294ca3533629fa1808
Johny-zu <angel_outlook.com>
2024-04-03 18:46:20 -0600

    Clase de punteros basico

---
+++ Bases/puntero.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int funcion(int valor, int incremento) {
+    valor = valor + incremento; // Se le suma el incremento al valor recibido como argumento
+    return valor;
+}
+
+int main() {
+    int numero = 10;
+    int incremento; // Incremento dinámico
+
+    cout << "Ingrese un valor para sumar: ";
+    cin >> incremento;
+
+    cout << "Antes de funcion: " << numero << endl; // Imprime 10
+
+    // Llamada a la función funcion, pasando el valor de numero y el incremento como argumentos
+    int resultado = funcion(numero, incremento);
+
+    cout << "Después de funcion: " << resultado << endl; // Imprime 15 (10 + incremento)
+
+    return 0;
+}
